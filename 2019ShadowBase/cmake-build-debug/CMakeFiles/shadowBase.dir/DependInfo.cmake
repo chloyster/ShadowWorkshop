ac08ce31b289245fbbf3bce45926ebccee867b0e
Christopher Gix <chrisgix97@gmail.com>
2019-04-18 15:36:21 -0700

    Completed workshop
    No gitignore so lots of ide garbage in here

---
+++ 2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/ext/glad/src/glad.c" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/ext/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../ext"
+  "../ext/glad/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/ext/tiny_obj_loader/tiny_obj_loader.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/GLSL.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/GLSL.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/MatrixStack.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/MatrixStack.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/Program.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/Program.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/Shape.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/Shape.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/Texture.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/Texture.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/WindowManager.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/WindowManager.cpp.o"
+  "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/src/main.cpp" "/home/chris/Documents/CalPoly/CSC-476/ShadowWorkshop/ShadowWorkshop/2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext"
+  "../ext/glad/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2019ShadowBase/cmake-build-debug/CMakeFiles/shadowBase.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shadowBase.dir/ext/glad/src/glad.c.o"
+  "CMakeFiles/shadowBase.dir/ext/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/GLSL.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/MatrixStack.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/Program.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/Shape.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/Texture.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/WindowManager.cpp.o"
+  "CMakeFiles/shadowBase.dir/src/main.cpp.o"
+  "shadowBase.pdb"
+  "shadowBase"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/shadowBase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
